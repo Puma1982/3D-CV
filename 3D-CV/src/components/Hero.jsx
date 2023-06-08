a4945b790d6f09dfc616757f2fda5e114bd34479
VIP-HACKER <griss_hai2005@hotmail.com>
2023-06-08 20:44:21 +0200

    Overview Done 1:10:02

--- 3D-CV/src/components/Hero.jsx
+++ 3D-CV/src/components/Hero.jsx
@@ -24,11 +24,10 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
-
+   {/**Mouse motion durattion 1.5 sec or 6 sec*/}
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            {/**Mouse motion durattion 1.5 sec or 6 sec*/}
             <motion.div
               animate={{
                 y: [0, 24, 0],
